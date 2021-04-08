03be4e077eb86fca93d49c9cb1366836e5370458
Jarrete Barnett <jarretebarnett@gmail.com>
2021-04-07 22:32:14 -0400

    Latest commit after trying to troubleshoot route and readFileSync problems. Input still not showing automatically in browser.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,6 +6,7 @@ const db = require("../db/db.json");
 router.get("/notes", (req, res) => {
     // console.log("hello");
     fs.readFileSync("./db/db.json", db);
+    // console.log(db);
     res.json(db);
 })
 
@@ -15,6 +16,7 @@ router.post("/notes", (req, res) => {
     addNote.id = uuid.v4();
     notes.push(addNote);
     fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+    // console.log(notes);
     res.json(notes);
 })
 
@@ -22,6 +24,7 @@ router.delete("/notes/:id", (req, res) => {
     const notes = JSON.parse(fs.readFileSync("./db/db.json"));
     const eraseNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
     fs.writeFileSync("./db/db.json", JSON.stringify(eraseNote));
+    
     res.json(eraseNote);
 })
 
